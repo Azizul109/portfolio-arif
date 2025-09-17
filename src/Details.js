44eaf095a9b894c2bcab4a7e076118889b3a3cd2
azizularif420 <arif@mazegeek.com>
2025-09-18 01:50:17 +0600

    changes on project list

--- src/Details.js
+++ src/Details.js
@@ -28,8 +28,22 @@ import projectImage1 from "./assets/projects/project1.png";
 import projectImage2 from "./assets/projects/project2.png";
 import projectImage3 from "./assets/projects/project3.png";
 import projectImage4 from "./assets/projects/project4.png";
-import projectImage5 from "./assets/projects/project5.jpg";
-import projectImage6 from "./assets/projects/project6.jpg";
+import projectImage5 from "./assets/projects/project5.png";
+import projectImage6 from "./assets/projects/project6.png";
+import projectImage7 from "./assets/projects/project7.png";
+import projectImage8 from "./assets/projects/project8.png";
+import projectImage9 from "./assets/projects/project9.png";
+import projectImage10 from "./assets/projects/project10.png";
+import projectImage11 from "./assets/projects/project11.png";
+import projectImage12 from "./assets/projects/project12.png";
+import projectImage13 from "./assets/projects/project13.png";
+import projectImage14 from "./assets/projects/project14.png";
+import projectImage15 from "./assets/projects/project15.png";
+import projectImage16 from "./assets/projects/project16.png";
+import projectImage17 from "./assets/projects/project17.png";
+import projectImage18 from "./assets/projects/project18.png";
+import projectImage19 from "./assets/projects/project19.png";
+import projectImage20 from "./assets/projects/project20.png";
 
 // Logos
 export const logos = {
@@ -126,58 +140,181 @@ export const techStackDetails = {
 // Enter your Project Details here
 export const projectDetails = [
   {
-    title: "Project title 1",
+    title: "Partscargo Website",
     image: projectImage1,
-    description: `This is sample project description random things are here in description This is sample
-project lorem ipsum generator for dummy content`,
-    techstack: "HTML/CSS, JavaScript",
-    previewLink: "https://google.com",
-    githubLink: "https://github.com",
+    description: `ecommerce website for auto parts with PayPal and Stripe integration. Order placed from website will go directly to Partzroot`,
+    techstack: "HTML/CSS, JavaScript, Laravel, Node",
+    previewLink: "https://partscargo.com/",
+    githubLink: "https://partscargo.com/",
   },
   {
-    title: "Project title 2",
+    title: "Partzroot Software",
     image: projectImage2,
-    description: `This is sample project description random things are here in description This is sample
-project lorem ipsum generator for dummy content`,
-    techstack: "HTML/CSS, JavaScript",
-    previewLink: "https://google.com",
-    githubLink: "https://github.com",
+    description: `in-house web application built from scratch capable of managing the entire auto parts order
+life cycle`,
+    techstack: "HTML/CSS, JavaScript, Laravel, Node",
+    previewLink: "https://admin.staging.partzroot.com/admin-login",
+    githubLink: "https://admin.staging.partzroot.com/admin-login",
   },
   {
-    title: "Project title 3",
+    title: "Maxxecom",
     image: projectImage3,
-    description: `This is sample project description random things are here in description This is sample
-project lorem ipsum generator for dummy content`,
-    techstack: "HTML/CSS, JavaScript",
-    previewLink: "https://google.com",
-    githubLink: "https://github.com",
+    description: `web application built to integrate eBay and Amazon stores to achieve centralized
+operational control`,
+    techstack: "HTML/CSS, JavaScript, Laravel, Node",
+    previewLink: "https://flawless.maxxecom.com/dashboard",
+    githubLink: "https://flawless.maxxecom.com/dashboard",
   },
   {
-    title: "Project title 4",
+    title: "Auto Parts Data Warehouse",
     image: projectImage4,
-    description: `This is sample project description random things are here in description This is sample
-project lorem ipsum generator for dummy content`,
-    techstack: "HTML/CSS, JavaScript",
-    previewLink: "https://google.com",
-    githubLink: "https://github.com",
+    description: `specialized database designed to store and manage a dataset relating to
+the auto parts industry`,
+    techstack: "HTML/CSS, JavaScript, Laravel, Node",
+    previewLink: "https://data.partscargo.com/home",
+    githubLink: "https://data.partscargo.com/home",
   },
   {
-    title: "Project title 5",
+    title: "UCSI University Bangladesh Branch Campus Website",
     image: projectImage5,
-    description: `This is sample project description random things are here in description This is sample
-project lorem ipsum generator for dummy content`,
-    techstack: "HTML/CSS, JavaScript",
-    previewLink: "https://google.com",
-    githubLink: "https://github.com",
+    description: `maintain, update and modify the Bangladesh branch
+campus website regularly`,
+    techstack: "HTML/CSS, JavaScript, PHP, Drupal",
+    previewLink: "https://www.bangladesh.ucsiuniversity.edu.my/",
+    githubLink: "https://github.com/Azizul109/ucsi-university",
   },
   {
-    title: "Project title 6",
+    title: "HRM",
     image: projectImage6,
-    description: `This is sample project description random things are here in description This is sample
-project lorem ipsum generator for dummy content`,
-    techstack: "HTML/CSS, JavaScript",
-    previewLink: "https://google.com",
-    githubLink: "https://github.com",
+    description: `software to handle human resource operation for the university collaborating with Malaysian
+development team`,
+    techstack: "HTML/CSS, JavaScript, Node",
+    previewLink:
+      "https://iisv2bd.ucsiuniversity.edu.my/bgd/f?p=510:43:10851602668650:::::",
+    githubLink:
+      "https://iisv2bd.ucsiuniversity.edu.my/bgd/f?p=510:43:10851602668650:::::",
+  },
+  {
+    title: "Student Portal Software",
+    image: projectImage7,
+    description: `software to manage the students admission process and overall payment,
+semester related coursework and many mare`,
+    techstack: "HTML/CSS, JavaScript, Node",
+    previewLink:
+      "https://iisv2bd.ucsiuniversity.edu.my/bgd/f?p=520:LOGIN_DESKTOP:14171855893794:::::IK",
+    githubLink:
+      "https://iisv2bd.ucsiuniversity.edu.my/bgd/f?p=520:LOGIN_DESKTOP:14171855893794:::::IK",
+  },
+  {
+    title: "Library Management Software",
+    image: projectImage8,
+    description: `Library management software done from scratch with RFID scanner device integration for World
+University Bangladesh`,
+    techstack: "HTML/CSS, JavaScript, React, Laravel, Java, Node",
+    previewLink: "https://library.wub.edu.bd/",
+    githubLink: "https://library.wub.edu.bd/",
+  },
+  {
+    title: "BIMRAD",
+    image: projectImage9,
+    description: `Journal submission software done from scratch to submit university research and publications for
+university`,
+    techstack: "HTML/CSS, JavaScript, React, Laravel, Node",
+    previewLink: "https://bimradbd.org/journal",
+    githubLink: "https://bimradbd.org/journal",
+  },
+  {
+    title: "BCS",
+    image: projectImage10,
+    description: `Bangladesh Computer Society website development with member portal management software
+development from scratch`,
+    techstack: "HTML/CSS, JavaScript, Vue, Laravel, Node",
+    previewLink: "https://bcsbd.org.bd/",
+    githubLink: "https://bcsbd.org.bd/",
+  },
+  {
+    title: "BEPZA",
+    image: projectImage11,
+    description: `develop the dynamic website of bepza with the backend management capability for website`,
+    techstack: "HTML/CSS, JavaScript, Vue, Laravel, Node",
+    previewLink: "https://bepza.gov.bd/",
+    githubLink: "https://bepza.gov.bd/",
+  },
+  {
+    title: "BASIS",
+    image: projectImage12,
+    description: `upgradation and modification of basis website and member portal management software to
+reduce the hassle for basis member`,
+    techstack: "HTML/CSS, JavaScript, Vue, Laravel, Node",
+    previewLink: "https://basis.org.bd/",
+    githubLink: "https://basis.org.bd/",
+  },
+  {
+    title: "E-CAB",
+    image: projectImage13,
+    description: `upgradation and modification of e-cab website and member portal management software to
+reduce the hassle for e-cab member`,
+    techstack: "HTML/CSS, JavaScript, Vue, Laravel, Node",
+    previewLink: "https://e-cab.net/",
+    githubLink: "https://e-cab.net/",
+  },
+  {
+    title: "MIST",
+    image: projectImage14,
+    description: `Military institute of science and technology civil engineering department website and dynamic
+backend management software development`,
+    techstack: "HTML/CSS, JavaScript, Vue, Laravel, Node",
+    previewLink: "https://ce-cats.mist.ac.bd/",
+    githubLink: "https://ce-cats.mist.ac.bd/",
+  },
+  {
+    title: "BIMRAD",
+    image: projectImage15,
+    description: `Bangladesh Institute of Maritime Research and Development website and dynamic backend
+management software development`,
+    techstack: "HTML/CSS, JavaScript, Vue, Laravel, Node",
+    previewLink: "https://bimradbd.org/",
+    githubLink: "https://bimradbd.org/",
+  },
+  {
+    title: "Feni University",
+    image: projectImage16,
+    description: `Feni University dynamic website development`,
+    techstack: "HTML/CSS, JavaScript, Vue, Laravel, Node",
+    previewLink: "https://feniuniversity.ac.bd/",
+    githubLink: "https://feniuniversity.ac.bd/",
+  },
+  {
+    title: "Definity Legend",
+    image: projectImage17,
+    description: `Gaming website design for Malaysian client, where main idea was to design the website with gaming vie`,
+    techstack: "HTML/CSS, JavaScript, React",
+    previewLink: "http://www.definitylegend.com/",
+    githubLink: "http://www.definitylegend.com/",
+  },
+  {
+    title: "Sylhet City Corporation",
+    image: projectImage18,
+    description: `Website for Sylhet City Corporation, where dynamic website is made and backend software to handle the frontend part`,
+    techstack: "HTML/CSS, JavaScript, PHP, Laravel",
+    previewLink: "https://sylhet-city-corporation.netlify.app/",
+    githubLink: "https://sylhet-city-corporation.netlify.app/",
+  },
+  {
+    title: "Firecommerce",
+    image: projectImage19,
+    description: `Ecommerce website where user can register, login and add item to cart for order, payment with Stripe and Paypal`,
+    techstack: "HTML/CSS, JavaScript, React, Node, Firbase, MongoDB",
+    previewLink: "https://firecommerce-arif.netlify.app/login",
+    githubLink: "https://firecommerce-arif.netlify.app/login",
+  },
+  {
+    title: "Food Recipe App",
+    image: projectImage20,
+    description: `API integration with third party to get the specific ingredients for a dish. Suppose include mango will show dish with mango`,
+    techstack: "HTML/CSS, JavaScript, React, Node, Firbase",
+    previewLink: "https://recipe-app-arif.netlify.app/",
+    githubLink: "https://recipe-app-arif.netlify.app/",
   },
 ];
 
