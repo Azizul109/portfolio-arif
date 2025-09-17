d3101bd169518a0a915adfb9df1a6bc2b3182db2
azizularif420 <arif@mazegeek.com>
2025-09-18 00:39:09 +0600

    commit of personal portfolio

---
+++ src/Details.js
@@ -0,0 +1,189 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/logo.png";
+import logo from "./assets/logo.png";
+// Profile Image
+import profile from "./assets/profile.jpg";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+import laravel from "./assets/techstack/laravel.png";
+import node from "./assets/techstack/node.png";
+import php from "./assets/techstack/php.png";
+
+// Porject Images
+import projectImage1 from "./assets/projects/project1.png";
+import projectImage2 from "./assets/projects/project2.png";
+import projectImage3 from "./assets/projects/project3.png";
+import projectImage4 from "./assets/projects/project4.png";
+import projectImage5 from "./assets/projects/project5.jpg";
+import projectImage6 from "./assets/projects/project6.jpg";
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Md Azizul Arif",
+  tagline: "I build software and website",
+  img: profile,
+  about: `Attributes - Attention to detail, Problem-solving, Leadership, Strong communication, Ability to work under pressure, Can do anything attitude.`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/md-azizul-arif-409548112/",
+  github: "https://www.github.com/",
+  gitlab: "https://gitlab.com/azizularif420",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Software Engineer",
+    Company: `Mazegeek Technologies Limited`,
+    Location: "Banani, Dhaka",
+    Type: "Full Time",
+    Duration: "May 2024 - Currently Working",
+  },
+  {
+    Position: "Software Engineer",
+    Company: `UCSI University Bangladesh Branch Campus`,
+    Location: "Banani, Dhaka",
+    Type: "Full Time/Part Time",
+    Duration: "Mar 2024 - Jun 2025",
+  },
+  {
+    Position: "Software Engineer",
+    Company: `Nogor Solutions Limited`,
+    Location: "Niketon, Gulshan-1, Dhaka",
+    Type: "Full Time",
+    Duration: "Sep 2022 - Feb 2024",
+  },
+  {
+    Position: "Junior Software Engineer",
+    Company: `Namespace IT`,
+    Location: "Uttara, Dhaka",
+    Type: "Full Time",
+    Duration: "Aug 2021 - Sep 2022",
+  },
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "Master's of Computer Science & Engineering",
+    Company: "East West University",
+    Location: "Dhaka",
+    Type: "Full Time",
+    Duration: "2021 - 2023",
+  },
+  {
+    Position: "Bachelor of Engineering in Software Engineering",
+    Company: `Sichuan Univeristy`,
+    Location: "China",
+    Type: "Full Time",
+    Duration: "2017 - 2021",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  sass: sass,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+  laravel: laravel,
+  node: node,
+  php: php,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Project title 1",
+    image: projectImage1,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 2",
+    image: projectImage2,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 3",
+    image: projectImage3,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 4",
+    image: projectImage4,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 5",
+    image: projectImage5,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 6",
+    image: projectImage6,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "azizularifnahim52@gmail.com",
+  website: "https://portfolio-arif.netlify.app/",
+  phone: "01726058935",
+};
